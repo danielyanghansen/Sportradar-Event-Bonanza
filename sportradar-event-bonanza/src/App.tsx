55049f20769313c18b86d60436bb95a69e40f32c
Daniel Yang Hansen <daniel.yang.hansen@gmail.com>
2022-10-21 15:24:19 +0200

    Add Spinning Custom logo and mapbox dependency

--- sportradar-event-bonanza/src/App.tsx
+++ sportradar-event-bonanza/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
+import logo1024 from './logo1024.png'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo1024} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
